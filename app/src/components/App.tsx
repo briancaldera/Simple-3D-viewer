8a86bf651d7af53278c14be0afb62ccfaa180424
Brian Caldera <brian_alex07@outlook.com>
2024-07-21 03:00:15 -0400

    Installed React

---
+++ app/src/components/App.tsx
@@ -0,0 +1,7 @@
+const App = () => {
+    return (
+        <main></main>
+    )
+}
+
+export default App
--- app/src/renderer.ts
+++ app/src/renderer.ts
@@ -29,3 +29,6 @@
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+
+// Add this to the end of the existing file
+import './app';
